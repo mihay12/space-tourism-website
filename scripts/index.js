dfbb062c5dd60cbda02050a4cf7bb39f7590e56b
mihay12 <mihailgaravsyuk@gmail.com>
2021-11-29 20:09:35 +0200

    transform circle title to upper case

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     // const navItems = $("nav span");
-    const navItems = $("nav span, .heading-5, .heading-1");
+    const navItems = $("nav span, .heading-5, .heading-1, .heading-4");
     for (let i = 0; i < navItems.length; i++) {
         const itemCode = navItems[i].innerHTML.toUpperCase();
         navItems[i].innerHTML = itemCode;
