1aaa92db3d24fdd0225981cb9cda47611afc1d90
mihay12 <mihailgaravsyuk@gmail.com>
2021-11-29 20:06:45 +0200

    finished first page

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 $(document).ready(() => {
-    const navItems = $("span");
-
+    // const navItems = $("nav span");
+    const navItems = $("nav span, .heading-5, .heading-1");
     for (let i = 0; i < navItems.length; i++) {
         const itemCode = navItems[i].innerHTML.toUpperCase();
         navItems[i].innerHTML = itemCode;
     }
+
+    $("nav span").addClass("nav-text");
 })
+
